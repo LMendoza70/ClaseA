bb994169c2db9eb135450e219272904d6dbda0f9
@LMendoza70 <luis.mendoza@uthh.edu.mx>
2025-05-19 07:15:48 -0600

    first commit

---
+++ ClaseA/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaseA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //voy a inicializar un objeto rectangulo
+            clsRectangulo miRectangulo = new clsRectangulo();
+            Console.WriteLine("Programita que calcula area y perimetro de un rectangulo");
+            Console.WriteLine("Captura la altura del rectangulo...");
+            miRectangulo.setAltura(float.Parse( Console.ReadLine()));
+            Console.WriteLine("Captura la base del rectangulo");
+            miRectangulo.setAncho(float.Parse(Console.ReadLine()));
+            Console.WriteLine("Segun los datos proporcionados ");
+            Console.WriteLine("El area del rectangulo es : "+ miRectangulo.Area()+" m2");
+            Console.WriteLine("El perimetro del rectangulo es : "+miRectangulo.Perimetro()+" m");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ClaseA/clsRectangulo.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaseA
+{
+    class clsRectangulo
+    {
+        //creamos los atributos 
+        private float Altura;
+        private float Ancho;
+
+        //definimos propiedades de los atributos (lectura y escritura)
+        public float getAltura()
+        {
+            return Altura;
+        }
+        public void setAltura(float valor)
+        {
+            Altura = valor;
+        }
+
+        public float getAncho() 
+        { 
+            return Ancho; 
+        }
+
+        public void setAncho(float valor)
+        {
+            Ancho = valor;
+        }
+
+        //creamos el constructor
+        public clsRectangulo()
+        {
+            Altura = 0;
+            Ancho = 0;
+        }
+
+        public clsRectangulo(float ba,float al)
+        {
+            Altura = al;
+            Ancho = ba;
+        }
+        //creamos metodo perimetro
+        public float Perimetro()
+        {
+            float res = 0;
+            //res = Altura * 2 + Ancho * 2;
+            //res = Altura + Altura + Ancho + Ancho;
+            res = (Altura + Ancho) * 2;
+            return res;
+        }
+
+        public float Area()
+        {
+            float res = 0;
+            res = Altura * Ancho;
+            return res;
+        }
+    }
+}
